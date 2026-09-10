69e16920fa7502a0eadac3059675250f2aaee9d2
임태후 <revival@gmail.com>
2026-09-10 20:14:57 +0900

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+function greet(name){
+    return `hello, ${name}`;
+}
+
+const message = greet("TypeScript");
+console.log(message);
---
+++ src/app.ts
@@ -0,0 +1,6 @@
+function greet(name: string): string{
+    return `hello, ${name}`;
+}
+
+const message = greet("TypeScript");
+console.log(message);
